c060d033dd84f71d5036b22617a72856b4755567
yozp <a1371361879@outlook.com>
2025-08-24 12:30:33 +0800

    UV统计-测试百万数据的统计（HyperLogLog）

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
 import java.util.concurrent.CountDownLatch;
@@ -24,6 +25,9 @@ class HmDianPingApplicationTests {
 
     @Resource
     private RedissonClient redissonClient;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -58,18 +62,37 @@ class HmDianPingApplicationTests {
      * 测试使用Redission的分布式锁
      */
     @Test
-    void testRedission() throws Exception{
+    void testRedission() throws Exception {
         //获取锁(可重入)，指定锁的名称
         RLock lock = redissonClient.getLock("anyLock");
         //尝试获取锁，参数分别是：获取锁的最大等待时间(期间会重试)，锁自动释放时间，时间单位
         boolean isLock = lock.tryLock(1, 10, TimeUnit.SECONDS);
         //判断获取锁成功
-        if(isLock){
-            try{
+        if (isLock) {
+            try {
                 System.out.println("执行业务");
-            }finally {
+            } finally {
                 lock.unlock();
             }
         }
+    }
+
+    @Test
+    void testHyperLogLog() {
+        //准备数据，装用户数据
+        String[] users = new String[100];
+        //数组角标
+        int index = 0;
+        for (int i = 1; i <= 1000000; i++) {
+            //赋值
+            users[index++] = "user_" + i;
+            //每1000条发送一次
+            if (i % 1000 == 0) {
+                index = 0;
+                stringRedisTemplate.opsForHyperLogLog().add("hll1", users);
+            }
+        }
+        Long size = stringRedisTemplate.opsForHyperLogLog().size("hll1");
+        System.out.println("size = " + size);
     }
 }
